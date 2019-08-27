978535656b9f30e3f146f4b69330d39b9914f117
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-08-27 17:03:02 +1000

    removed the print icons

---
+++ combinatorialLogic.js
@@ -0,0 +1,22 @@
+const State = {
+    both: {
+        showAllTitles: false,
+        showDescriptions: true,
+        showProcesses: true
+    },
+    processOnly: {
+        showAllTitles: false,
+        showDescriptions: true,
+        showProcesses: false
+    },
+    policyOnly: {
+        showAllTitles: true,
+        showDescriptions: false,
+        showProcesses: true
+    },
+    none: {
+        showAllTitles: true,
+        showDescriptions: false,
+        showProcesses: false
+    }
+}
